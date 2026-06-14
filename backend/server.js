c15bf71c3fd9bf8f9bb86762c3f0731bc8e5eec3
rahman <safdar23@if.unjani.ac.id>
2026-06-14 10:09:03 +0700

    feath: vercel json

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const app = require('./src/app');
 const { PORT } = require('./src/config/env');
 
-app.listen(PORT, () => {
-  console.log(`Webservice berjalan di http://localhost:${PORT}`);
-});
+// Hanya jalankan listener jika tidak di-deploy sebagai Vercel Serverless Function
+if (process.env.NODE_ENV !== 'production' || !process.env.VERCEL) {
+  app.listen(PORT, () => {
+    console.log(`🚀 Webservice berjalan di http://localhost:${PORT}`);
+  });
+}
+
+// Export module untuk Vercel Serverless
+module.exports = app;
